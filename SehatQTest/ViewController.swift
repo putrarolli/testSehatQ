e0d3db8b6dd49f18fc104f6eea84b743adadfd28
putrarolli <putra.rolli93@gmail.com>
2019-09-22 01:01:22 +0700

    Initial Commit

---
+++ SehatQTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SehatQTest
+//
+//  Created by putra rolli on 22/09/19.
+//  Copyright © 2019 putra rolli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
