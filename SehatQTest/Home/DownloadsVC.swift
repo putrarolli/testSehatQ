8509bd8db62ca5a5a7786c32086f3d59b80e2f47
putrarolli <putra.rolli93@gmail.com>
2019-09-22 23:08:43 +0700

    first commit

---
+++ SehatQTest/Home/DownloadsVC.swift
@@ -0,0 +1,9 @@
+//
+//  downloadsVC.swift
+//  SehatQTest
+//
+//  Created by putra rolli on 22/09/19.
+//  Copyright © 2019 putra rolli. All rights reserved.
+//
+
+import Foundation
---
+++ SehatQTest/Login/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  SehatQTest
+//
+//  Created by putra rolli on 22/09/19.
+//  Copyright © 2019 putra rolli. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
